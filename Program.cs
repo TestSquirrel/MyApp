ade3c3bc1fad60f32c8350af09107dfcf7894be9
Sameer2810 <38692494+Sameer2810@users.noreply.github.com>
2019-09-05 16:30:19 +0530

    eight commit

--- Program.cs
+++ Program.cs
@@ -30,7 +30,6 @@ namespace MyApp
             //{
             //    await mgr.UpdateApp();
             //}
-            //var
             try
             {
                 using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/Sameer2810/TestSquirrel"))
